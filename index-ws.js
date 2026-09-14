40d9f26d88c349b7e2de72d77afbd491af74916d
MritunjayTiwari14 <shreyashtiwari14.11@gmail.com>
2026-09-14 13:32:23 +0530

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -26,6 +26,19 @@ const WebSocketServer = require('ws').Server;
 // attach the web socketserver to the existing
 const wss = new WebSocketServer({server: server});
 
+// use a process listener, whenever a server closes then shutdown the db along with gracefully closing server
+process.on('SIGINT', () => {
+
+	// close the websocket also connection for each client
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		// close the database connection
+		shutdownDB();
+	})
+});
+
 // The function is passed the connected web socket connection, not the whole library
 wss.on('connection', function connection(ws) {
 	// wss has a property clients
@@ -39,9 +52,13 @@ wss.on('connection', function connection(ws) {
 	if (ws.readyState === ws.OPEN) {
 		ws.send('Welcome to my server');
 	}
+	
+	db.run(`INSERT INTO visitors (count, time)	
+		VALUES (${numClient}, datetime('now'))
+	`);
 
 	ws.on('close', function close() {
-		wss.broadcast(`Current visitors : ${numClient}`);
+		wss.broadcast(`Current visitors : ${wss.clients.size}`);
 		console.log('A client has disconnected');
 	});
 
@@ -52,8 +69,38 @@ wss.broadcast = function broadcast(data) {
 
 	// for each client connected to the websocket server send the data
 	wss.clients.forEach(function each(client) {
-
 		// send data to each client
 		client.send(data);
 	});
+}
+
+
+/** end websockets */
+
+/** begin database */
+const sqlite = require('sqlite3');
+
+// store the db in memory
+const db = new sqlite.Database(':memory:');
+
+// ensure the database is created before any command is setup
+db.serialize(() => {
+	db.run(`
+		CREATE TABLE visitors (
+			count INTEGER,
+			time TEXT
+		)
+	`)
+});
+
+function getCounts() {
+	db.each("SELECT * FROM visitors", (err, row) => {
+		console.log(row);
+	});
+}
+
+function shutdownDB() {
+	getCounts();
+	console.log('Shutting down db');
+	db.close();
 }
